eab3aa354d9b0767ca669cd540a1b4ddc12df9a4
deekondajashwanth767587-commits <deekondajashwanth767587@gmail.com>
2026-03-23 20:56:39 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Length of Longest Cycle in a Graph/length-of-longest-cycle-in-a-graph.java
+++ Difficulty: Hard/Length of Longest Cycle in a Graph/length-of-longest-cycle-in-a-graph.java
@@ -1,47 +1,31 @@
-import java.util.*;
-
 class Solution {
     public int longestCycle(int V, int[][] edges) {
-        int[] next = new int[V];
-        Arrays.fill(next, -1);
-
-        for (int[] e : edges) {
-            next[e[0]] = e[1];
-        }
-
+        // code here
+        int[] next= new int[V];
+        Arrays.fill(next , -1);
+        for(int[] e : edges)next[e[0]]=e[1];
         boolean[] visited = new boolean[V];
-        int max = -1;
-
-        for (int i = 0; i < V; i++) {
-            if (visited[i]) continue;
-
-            Map<Integer, Integer> stepMap = new HashMap<>();
-            List<Integer> path = new ArrayList<>();
-
-            int node = i;
-            int step = 0;
-
-            while (node != -1 && !visited[node]) {
-
-                if (stepMap.containsKey(node)) {
-                    int cycleLength = step - stepMap.get(node);
-                    max = Math.max(max, cycleLength);
-                    break;
+        int max=-1;
+        for(int i=0;i<V;i++){
+            if(!visited[i]){
+                int curr=i;
+                Map<Integer, Integer>map = new HashMap<>();
+                int step=0;
+                while(curr!=-1 && !visited[curr]){
+                    visited[curr]=true;
+                    map.put(curr, step++);
+                    curr = next[curr];
+                    
+                    if(curr!=-1 && map.containsKey(curr)){
+                        int cyclelength= step-map.get(curr);
+                        max = Math.max(max , cyclelength);
+                        break;
+                        
+                    }
                 }
-
-                stepMap.put(node, step);
-                path.add(node);
-
-                step++;
-                node = next[node];
-            }
-
-            // mark all nodes in this traversal as visited
-            for (int n : path) {
-                visited[n] = true;
             }
         }
-
         return max;
+        
     }
 }
