24dc73a18a2eac8c4315c9fa61ccc64a2b31ae83
deekondajashwanth767587-commits <deekondajashwanth767587@gmail.com>
2026-04-29 21:37:36 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Min Swaps to Group 1s/min-swaps-to-group-1s.java
+++ Difficulty: Medium/Min Swaps to Group 1s/min-swaps-to-group-1s.java
@@ -1,33 +1,36 @@
 class Solution {
     public int minSwaps(int[] arr) {
-        int result = 0;
-        int freq = 0;
-
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] == 1) freq++;
+        // code here
+        // in note book
+        int result =0;
+        int freq=0;
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]==1)freq++;
+            
         }
-
-        if (freq == 0) return -1;
-
-        int window = 0;
-        int left = 0;
-        int count1 = 0;
-
-        for (int i = 0; i < arr.length; i++) {
+        if(freq==0)return -1;
+        int window =0;
+        int left=0;
+        int count1=0;
+        for(int i=0;i<arr.length;i++){
             window++;
-            if (arr[i] == 1) count1++;
-
-            if (window > freq) {
-                if (arr[left] == 1) count1--; // update only if needed
-                left++;                       // ALWAYS move left
+            if(arr[i]==1)count1++;
+             
+            if(window>freq){
+                if(arr[left]==1)count1--;
+                left++;
                 window--;
+                     
+                 
+            
             }
-
-            if (window == freq) {
-                result = Math.max(result, count1);
+            
+            if(window==freq){
+                result =Math.max(result , count1);
             }
         }
-
-        return freq - result;
+        
+        return  freq-result;
+    
     }
 }
