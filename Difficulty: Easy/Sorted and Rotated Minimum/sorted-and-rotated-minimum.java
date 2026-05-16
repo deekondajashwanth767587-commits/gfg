6af2aad9edd585e82a1f30d79bd780d4a1359474
deekondajashwanth767587-commits <deekondajashwanth767587@gmail.com>
2026-05-16 13:43:41 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Sorted and Rotated Minimum/sorted-and-rotated-minimum.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int findMin(int[] arr) {
+        // code here
+        int left =0;
+        int right =arr.length-1;
+        while(left<right){
+            int mid = (left+right)/2;
+            if(arr[mid]>arr[right]){
+                left=mid+1;
+            }else{
+                right=mid;
+            }
+            
+        }
+        return arr[left];
+    }
+}
