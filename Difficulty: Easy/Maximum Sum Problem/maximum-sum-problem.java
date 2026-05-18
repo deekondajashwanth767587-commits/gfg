45d0e2bb75d8a6689de29a06261c56f94482eb86
deekondajashwanth767587-commits <deekondajashwanth767587@gmail.com>
2026-05-18 14:30:52 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Maximum Sum Problem/maximum-sum-problem.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int maxSum(int n) {
+        // code here.
+        if(n==0) return 0;
+        return Math.max(
+            n,maxSum(n/2) + maxSum(n/3) + maxSum(n/4) );
+    }
+}
